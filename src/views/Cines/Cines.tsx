c0494aced4d65a7da29746d17c3c623cb66107b0
Saray Magallanes <smagarodri@gmail.com>
2023-12-13 11:01:34 +0100

    diseño contacto y pagina cines

--- src/views/Cines/Cines.tsx
+++ src/views/Cines/Cines.tsx
@@ -3,15 +3,20 @@ import './Cines.css'
 import { SeleccionCine } from '../../components/Cine/SeleccionCine.tsx';
 import { Header } from '../../components/Header/Header.tsx';
 import { Footer } from '../../components/Footer/Footer.tsx';
+import { TitleContainer } from '../../components/Home/TitleContainer.tsx';
+
 
 function Cines(): React.JSX.Element {
     return (
         <>
-        <Header/>
-        <div className='main'>
-            <SeleccionCine/>
+            <Header />
+            <div className='main'>
+            <div className='titulo'>
+                    <TitleContainer title='Cines' />
+                </div>
+                <SeleccionCine />
             </div>
-        <Footer/>
+            <Footer />
         </>
     );
 }
