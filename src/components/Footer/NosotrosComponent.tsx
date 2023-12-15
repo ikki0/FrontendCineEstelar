03c20f348dc1624d45911d7701900f0d9c104ef6
Saray Magallanes <smagarodri@gmail.com>
2023-12-15 09:57:10 +0100

    retoques estética

--- src/components/Footer/NosotrosComponent.tsx
+++ src/components/Footer/NosotrosComponent.tsx
@@ -1,10 +1,11 @@
+import { TitleContainer } from "../Home/TitleContainer";
 import "./NosotrosComponent.css";
 
 function NosotrosComponent(): React.JSX.Element {
   return (
     <>
       <header>
-        <h1 className="titulo">¿Por qué nosotros?</h1>
+        <TitleContainer title='¿Por qué nosotros?'/>
       </header>
       <article className="article">
         <p>
@@ -14,28 +15,28 @@ function NosotrosComponent(): React.JSX.Element {
         </p>
         <ol>
           <li>
-            <b>Entretenimiento con Propósito: </b>Disfruta de las mejores
+            <b>1. Entretenimiento con Propósito: </b>Disfruta de las mejores
             películas mientras contribuyes al bienestar global. El 5% del precio
             de tus entradas se destina a proyectos que marcan la diferencia.
             Colaboramos activamente con Save the Children y Reforesta, apoyando
             iniciativas para la infancia y la preservación del medio ambiente.
           </li>
           <li>
-            <b>Cine con Conciencia:</b> Dos veces al mes, te invitamos a unirte
+            <b>2. Cine con Conciencia:</b> Dos veces al mes, te invitamos a unirte
             a nuestra misión proyectando películas relacionadas con los 17
             Objetivos de Desarrollo Sostenible (ODS) establecidos por las
             Naciones Unidas. Desde igualdad de género hasta cambio climático,
             exploramos temas vitales que impactan nuestro mundo.
           </li>
           <li>
-            <b>Experiencia Única:</b> En CineEstelar, no solo vas a ver una
+            <b>3. Experiencia Única:</b> En CineEstelar, no solo vas a ver una
             película, vivirás una experiencia. Nuestras instalaciones están
             diseñadas para tu comodidad y disfrute. Además, cada boleto
             adquirido es un paso más hacia un futuro más brillante para las
             generaciones venideras.
           </li>
           <li>
-            <b>Compromiso con la Comunidad: </b>Nos preocupamos por nuestra
+            <b>4. Compromiso con la Comunidad: </b>Nos preocupamos por nuestra
             comunidad. A través de eventos especiales, colaboraciones locales y
             actividades educativas, nos esforzamos por ser un motor de cambio en
             nuestra área.
